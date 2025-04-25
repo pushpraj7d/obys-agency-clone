37c9a8e15995c0e065f075a42ce20a8c1c984841
pushpraj7d <singhpushparaj123@gmail.com>
2025-04-25 23:15:27 +0530

    Added new features and updates

--- script.js
+++ script.js
@@ -94,16 +94,111 @@ tl.from("#hero1", {
 }, "-=1.2")
 }
 function cursorAnimation(){
-    document.addEventListener("mousemove",function(dets){ //pure doccument pe jaha mouse move krega wha ki location mil jayegi
-        gsap.to("#crsr" ,{                  //location jaha milegi wha animation kr dena cursur div ka 
-            left:dets.x,                        
-            top:dets.y
-        })
-        
+    Shery.mouseFollower({
+        skew: true,
+        ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+        duration: 0.5,
+      });
+    var video = document.querySelector("#video-container video");
+    var videoContainer = document.querySelector("#video-container");
+    videoContainer.addEventListener("mouseenter",function(){
+        videoContainer.addEventListener("mousemove",function(dets){
+            gsap.to(".mousefollower",{
+                opacity:0
+            });
+            gsap.to("#video-cursor",{
+                left: dets.x - 430,
+                top: dets.y  -170
+            });
+        });
     });
-    
+   
+    videoContainer.addEventListener("mouseleave",function(){
+        gsap.to("#video-cursor",{
+            top:"-10%",
+            left:"70%"
+        });
+        gsap.to(".mousefollower",{
+            opacity:1
+        })
+    });
+
+
+
+
+    var flag = 0;
+    videoContainer.addEventListener("click",function(){
+        if(flag==0){
+            video.play();
+            video.style.opacity = 1;
+            document.querySelector("#video-cursor").innerHTML = `<i class="ri-pause-large-fill"></i>`;
+            gsap.to("#video-cursor",{
+                scale:0.5
+            })
+            flag = 1;
+        }
+        else{
+            video.pause();
+            video.style.opacity = 0;
+            document.querySelector("#video-cursor").innerHTML = `<i class="ri-play-large-fill"></i>`;
+            gsap.to("#video-cursor",{
+                scale:1
+            })
+            flag = 0;
+        }
+       
+    })
     Shery.makeMagnet("#nav-part2 h4");   //we used shery js librery here 
 }
+function sheryAnimation(){
+    Shery.imageEffect(".image-div", {
+        style: 6,
+        config: {"noiseDetail":{"value":7.44,"range":[0,100]},"distortionAmount":{"value":2.98,"range":[0,10]},"scale":{"value":36.36,"range":[0,100]},"speed":{"value":0.79,"range":[0,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":0.6666666666666666},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":false},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":2.01,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":0},"noise_speed":{"value":0.61,"range":[0,10]},"metaball":{"value":0.47,"range":[0,2]},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.47,"range":[0,2]},"noise_scale":{"value":7.63,"range":[0,100]}},
+        gooey: true,
+      });
+}
+function flagAnimation(){
+    document.addEventListener("mousemove",function(dets){
+        gsap.to("#flag",{
+            x:dets.x,
+            y:dets.y,
+            xPercent: -50,
+            yPercent: -50
+        })
+    })
+    
+    document.querySelector("#hero3").addEventListener("mouseenter",function(){
+        gsap.to("#flag",{
+            opacity:1
+        })
+    })
+    document.querySelector("#hero3").addEventListener("mouseleave",function(){
+        gsap.to("#flag",{
+            opacity:0
+        })
+    })
+}
+
 loadingAnimation(); //loading animation function ko call kiya
-// cursorAnimation();  //cursor animation function ko call kiya
+cursorAnimation();  //cursor animation function ko call kiya
 locomotiveAnimation(); //locomotive scrolltriggeer wala animation call kiya 
+sheryAnimation();   //gooey effect is se aya h 
+flagAnimation();
+
+var textAnimate = document.querySelector("#footer h1");
+textAnimate.addEventListener("mouseenter",function(){
+    gsap.to("#footer h1",{
+        fontFamily: 'Silk Serif',   //pehela ki ye change nhi ho rhi h
+        onStart:function(){
+            $('#footer h1').textillate({ in: { effect: 'fadeIn' } });
+        }
+    })
+})
+
+textAnimate.addEventListener("mouseleave", function () {  //dusra ki ye nhi kr rha work
+    gsap.to("#footer h1", {
+        onStart: function () {
+            $('#footer h1').textillate({ in: { effect: 'fadeOut' } }); 
+        }
+    });
+});
